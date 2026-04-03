c7711453a58a7ca60e7d8e1315fa08cbec75cc1b
youknowwgit <choiyh2001@naver.com>
2026-04-03 17:12:53 +0900

    feat: #2 Header&App 초기 제작 (#9)

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-warning navbar-dark">
+    <span class="navbar-brand ps-2"> 🌸 봄 플젝 벚꽃 말고 🌸</span>
+    <button
+      class="navbar-toggler"
+      type="button"
+      @click="isNavShow = !isNavShow"
+    >버튼
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      :class="
+        isNavShow ? 'collapse navbar-collapse show' : 'collapse navbar-collapse'
+      "
+    >
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">홈</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/team">팀 소개</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/teamrule">팀 규칙</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/teammate">팀원 소개</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script setup>
+import { ref } from 'vue';
+const isNavShow = ref(false);
+</script>
